57062354cad74a045ca4b41d9e0f5a7295ddd323
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2024-09-05 16:40:09 +0900

    a

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "./src",
+  base: process.env.BUILD_BASE ?? "/",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
